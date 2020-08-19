1c8a38f1003ee22b965a305e919783d91b51bb2f
Jim Armbruster <JMArmbruster@gmail.com>
2020-08-19 14:57:09 -0400

    boiler plate code for tests

---
+++ __tests__/db.js
@@ -0,0 +1,29 @@
+const booksDb = require('../books/booksModel.js');
+const customersDb = require('../customers/customersModel.js');
+const path = require('path');
+const fs = require('fs');
+
+const testBooksJsonFile = path.resolve(__dirname, '../books/booksModel.test.json');
+const testCustomersJsonFile = path.resolve(__dirname, '../customers/customersModel.test.json');
+
+describe('booksDb unit tests', () => {
+  beforeAll((done) => {
+    fs.writeFile(testBooksJsonFile, JSON.stringify([]), () => {
+      booksDb.reset();
+      done();
+    });
+  });
+
+  afterAll((done) => {
+      fs.writeFile(testBooksJsonFile, JSON.stringify([]), done);
+  });
+
+  describe('#sync', () => {
+    it('writes a new book to the JSON file', () => {
+      const book = [{title: 'Hello', author: 'World', numberOfPages: 400, publisher: 'Jimco', bookId: 0}];
+      const result = booksDb.create(book);
+      expect(result).not.toBeInstanceOf(Error)
+    });
+  })
+
+})
---
+++ __tests__/enzyme.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16;'
+import toJson from 'enzyme-to-json';
+
+import Book from '../MockFrontEnd/component/Book.jsx'
+import LeftContainer from '../MockFrontEnd/containers/LeftContainer.jsx'
+import App from '../MockFrontEnd/App.jsx'
