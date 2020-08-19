2bc22cff4c764c3e65d554368e2a767a53f7189e
Jim Armbruster <JMArmbruster@gmail.com>
2020-08-19 15:04:49 -0400

    more boiler plate for enzyme tests

--- __tests__/enzyme.js
+++ __tests__/enzyme.js
@@ -6,3 +6,14 @@ import toJson from 'enzyme-to-json';
 import Book from '../MockFrontEnd/component/Book.jsx'
 import LeftContainer from '../MockFrontEnd/containers/LeftContainer.jsx'
 import App from '../MockFrontEnd/App.jsx'
+
+configure({ adapter: new Adapter() });
+
+describe('React unit tests', () => {
+  describe('Book', () => {
+    let wrapper;
+    const props = {
+      
+    }
+  })
+})
